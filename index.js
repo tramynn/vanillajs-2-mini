6f0db604c7e253c2405cd88b1dab3bb8792e4100
Tramy Nguyen <tramynn2@gmail.com>
2019-08-14 11:57:14 -0500

    added a checked box

--- index.js
+++ index.js
@@ -3,7 +3,29 @@ document.querySelector("form").addEventListener("submit", addToDo);
 
 function addToDo(event) {
     event.preventDefault();
-    let toDo = document.createElement("li");
-    toDo.innerText = document.getElementById("item").value;
-    let list = document.querySelector("ul").appendChild(toDo);
+    let item = document.createElement("li");
+    item.innerText = document.getElementById("item").value;
+    item.addEventListener("click", completeToDo);
+
+    let button = document.createElement("button");
+    button.innerText = "x";
+    button.addEventListener("click", removeToDo);
+    item.append(button);
+
+    let list = document.querySelector("ul");
+    list.appendChild(item);
 } 
+
+function removeToDo(event) {
+    // since button is child of the item, use code to remove entire list
+    event.target.parentNode.remove();
+}
+
+function completeToDo(event) {
+    let value = event.target.getAttribute("aria-checked");
+    if (value !== "true") {
+        event.target.setAttribute("aria-checked", "true");
+    } else {
+        event.target.setAttribute("aria-checked", "false");
+    }
+}
